8fd1611eca7674463ac40fd006e0805d1e22f8b3
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-10-29 15:48:30 +0500

    updated with carousal slider in future builder

--- tubi/lib/Screens/home-view.dart
+++ tubi/lib/Screens/home-view.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:tubi/API%20Services/api-model.dart';
 import 'package:tubi/API%20Services/api-service.dart';
 import 'package:http/http.dart' as http;
@@ -15,7 +17,6 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   API_Service apiservice = API_Service();
   final String baseurl = "https://api.themoviedb.org/3/movie/popular?api_key=6ee85343f0516bb3cccc88b483019033";
- 
 
   List<Movie_Model> resposedata = [];
 
@@ -41,13 +42,15 @@ class _HomeViewState extends State<HomeView> {
       body: FutureBuilder(
         future: getApi(),
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: resposedata.length,
-                itemBuilder: (context, index) {
-                  var data = resposedata[index];
-                  return ListTile(title: Text(data.title ?? ""));
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CarouselSlider(
+              items: resposedata.map((movie) {
+                return Builder(builder: (BuildContext context) {
+                  return Container();
                 });
+              }).toList(),
+              options: CarouselOptions(),
+            );
           }
           return const CircularProgressIndicator();
         },
@@ -55,3 +58,35 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 }
+
+// items: resposedata.map((movie) {
+//   return Builder(
+//     builder: (BuildContext context) {
+//       return Container(
+//         width: MediaQuery.of(context).size.width,
+//         margin: const EdgeInsets.symmetric(horizontal: 5.0),
+//         decoration: BoxDecoration(
+//           color: Colors.amber,
+//         ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             // Display the movie poster using the poster path
+//             Image.network(
+//               'https://image.tmdb.org/t/p/w500${movie.posterPath}', // This accesses the posterPath field of the movie
+//               fit: BoxFit.cover,
+//               width: double.infinity,
+//               height: 300, // Adjust height as needed
+//             ),
+//             SizedBox(height: 10),
+//             Text(
+//               movie.title, // Accesses the title field of the movie
+//               style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+//             ),
+//             Text(movie.overview, textAlign: TextAlign.center), // Accesses the overview field of the movie
+//           ],
+//         ),
+//       );
+//     },
+//   );
+// }).toList(),
